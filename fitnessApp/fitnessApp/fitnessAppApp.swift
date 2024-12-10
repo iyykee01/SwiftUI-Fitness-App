043159600ed878d976b5ee0fb7fb7705655224e9
Ikenna udokporo <iyykee01@gmail.com>
2024-12-10 23:43:02 +0100

    initial comit

---
+++ fitnessApp/fitnessApp/fitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnessAppApp.swift
+//  fitnessApp
+//
+//  Created by Ikenna Udokporo on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct fitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
